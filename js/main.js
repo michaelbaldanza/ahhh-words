8f9f875704d6be65f540a6c43d815c48ed1e247f
Michael Baldanza <michael.snider.baldanza@gmail.com>
2021-05-13 21:56:22 -0400

    lay out board

--- js/main.js
+++ js/main.js
@@ -1,2 +1,23 @@
-const tiles = new Array(15*15).fill('');
+const squares = new Array(15*15).fill('');
 
+const board = document.getElementById('board');
+
+function createSquares() {
+  for (i = 0; i < squares.length; i++) {
+    let square = document.createElement('div');
+    square.setAttribute('squareNo', i.toString());
+    square.setAttribute('class', 'tile');
+    square.textContent = i.toString();
+    board.appendChild(square);
+    if (i === 0 || i % 15 === 0) {
+      square.setAttribute('pos', 'start');
+      square.textContent = 'start';
+    }
+    if ((i + 1) % 15 === 0) {
+      square.setAttribute('pos', 'end');
+      square.textContent = 'end';
+    }
+  }
+}
+
+createSquares();
